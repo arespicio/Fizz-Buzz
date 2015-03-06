0ff4435a1d1a0d989072175af6ab451320b3f243
Ashleigh Respicio <ashleighrespicio@gmail.com>
2015-03-06 10:14:53 -0500

    added function to display in DOM

--- app.js
+++ app.js
@@ -1,8 +1,29 @@
-var numberCount = 100;
-while (numberCount >= 0 ) {
-  console.log(numberCount);
-  numberCount--;
-  if (numberCount === 100){break};
+function myFunction() {
+	var text = "";
+    var i;
+    for (i=0; i<=100; i++) 
+    {
+    	if(i%3==0 && i%5==0) {
+    		console.log("FizzBuzz");
+    		text += "FizzBuzz <br/>";
+    	}
+
+        else if (i%3===0) {
+        	console.log("Fizz");
+        	text += "Fizz <br/>";
+        }
+
+        else if (i%5===0) {
+        	console.log("Buzz");
+        	text += "Buzz <br/>";
+        }
+        else {
+        	console.log(i);
+        	text += i + "<br/>" 
+    	}
+    }
+    // document.getElementById("display").innerHTML = text;
+    $('#display').append(text);
 }
 
 
