bfb0e878cd903e3746073731f02c661b80e9bd1c
Ashleigh Respicio <ashleighrespicio@gmail.com>
2015-03-11 21:54:53 -0400

    relocated function outside of document.ready

--- app.js
+++ app.js
@@ -1,3 +1,8 @@
+
+var text = "";
+var i;
+
+
 $(document).ready(function(){
     var number = prompt("Enter a number");
         if (number % 1 !== 0) {
@@ -7,38 +12,36 @@ $(document).ready(function(){
         };
     var num = + number;
 
-
-function myfunction(max) {
-	var text = "";
-    var i;
-    for (i=0; i<= max; i++) 
-    {
-    	if(i%3==0 && i%5==0) {
-    		console.log("FizzBuzz");
-    		text += "FizzBuzz <br/>";
-    	}
-
-        else if (i%3===0) {
-        	console.log("Fizz");
-        	text += "Fizz <br/>";
-        }
-
-        else if (i%5===0) {
-        	console.log("Buzz");
-        	text += "Buzz <br/>";
-        }
-        else {
-        	console.log(i);
-        	text += i + "<br/>" 
-    	}
-    }
+    printFizzBuzz(num);
 
     // document.getElementById("display").innerHTML = text;
     $('#display').append(text);
-}
-
-myfunction(num);
 
 });
 
+
+function printFizzBuzz(max) {
+
+    for (i=0; i<= max; i++) 
+    {
+        if(i%3==0 && i%5==0) {
+            console.log("FizzBuzz");
+            text += "FizzBuzz <br/>";
+        }
+
+        else if (i%3===0) {
+            console.log("Fizz");
+            text += "Fizz <br/>";
+        }
+
+        else if (i%5===0) {
+            console.log("Buzz");
+            text += "Buzz <br/>";
+        }
+        else {
+            console.log(i);
+            text += i + "<br/>" 
+        }
+    }
+}
 
